e857cc3be1dd7caafd7b79cbfcd3ab4d5f089ce1
CodeKhoi <30888184+CodeKhoi@users.noreply.github.com>
2017-12-15 12:29:18 -0800

    added style, idgb API to js file and changed some HTML

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
-$.ajax({
-    url: 'http://localhost:3000/igdb'
-}).done(function(data) {
-    console.log('data', data);
-    // $('.content').empty();
-    // for (i = 0; i < data.length; i++) {
-    //     $('.content').append('<div>Name: ' + data[i].name + '</div>');
-    // }
-    console.log(data);
-})
+
+$(document).ready(function() {
+
+	$('#searchContent').hide();
+
+	$.ajax({url: 'http://localhost:3000/igdb'})
+		.done(function(data) {
+		$('#submitUserData').on('click', function(event) {
+			$('#searchContent').empty(); //clear div of old content.
+			event.preventDefault(); //stops page from refreshing
+			var $earch = $('#userInput').val().trim(); //capture value
+			$('#searchContent').slideDown(1000);
+			var $earchResult = $('<div>');
+			for (var i = 0; i < data.length; i++) {
+				var $earchReturn = $('<p>');
+				$earchReturn.text(data[i].name);
+				$earchResult.append($earchReturn);
+				console.log(data[i].name)
+			}
+			$('#searchContent').append($earchResult);//append html to #searchContent div.
+			
+		});
+	})
+}); //End of document ready
