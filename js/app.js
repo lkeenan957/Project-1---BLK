35d37b62dc8ba713ea1a25982db5a954f312854f
CodeKhoi <30888184+CodeKhoi@users.noreply.github.com>
2017-12-17 21:17:37 -0800

    I created another html file for the login. Added modals for both sign in and signup

--- js/app.js
+++ js/app.js
@@ -8,17 +8,18 @@ $(document).ready(function() {
 		$('#submitUserData').on('click', function(event) {
 			$('#searchContent').empty(); //clear div of old content.
 			event.preventDefault(); //stops page from refreshing
-			var $earch = $('#userInput').val().trim(); //capture value
+			var search = $('#userInput').val().trim(); //capture value
 			$('#searchContent').slideDown(1000);
-			var $earchResult = $('<div>');
+			var searchResult = $('<div>');
 			for (var i = 0; i < data.length; i++) {
-				var $earchReturn = $('<p>');
-				$earchReturn.text(data[i].name);
-				$earchResult.append($earchReturn);
+				var searchReturn = $('<p>');
+				searchReturn.text(data[i].name);
+				searchResult.append(searchReturn);
 				console.log(data[i].name)
 			}
-			$('#searchContent').append($earchResult);//append html to #searchContent div.
-			
+			$('#searchContent').append(searchResult);//append html to #searchContent div.
 		});
 	})
+
 }); //End of document ready
+
